7f48616fd5df955a79705e08c0e3570cc1b8c1aa
Jeongbin Kang <kangbin8109@gmail.com>
2025-12-29 15:47:18 +0900

    blog preview card (Frontend Mentor #2)

---
+++ blog-card/src/components/BlogCard.jsx
@@ -0,0 +1,28 @@
+import "./BlogCard.css";
+import blogimage from "../assets/images/illustration-article.svg";
+import blogname from "../assets/images/image-avatar.webp";
+
+const BlogCard = () => {
+  return (
+    <article className="BlogCard">
+      <div className="blog_image">
+        <img src={blogimage} />
+      </div>
+      <div className="blog_content">
+        <span className="tag">Learning</span>
+        <time dateTime="2023-12-21">Published 21 Dec 2023</time>
+        <h1>HTML & CSS foundations</h1>
+        <p>
+          These languages are the backbone of every website, defining structure,
+          content, and perssentation.
+        </p>
+      </div>
+      <div className="author">
+        <img src={blogname} />
+        <span>Greg Hooper</span>
+      </div>
+    </article>
+  );
+};
+
+export default BlogCard;
